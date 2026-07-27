fc0970400ed3bde8989b2a7263a9c24d7a4cb498
parkjieun87 <dksxlzhd0807@naver.com>
2026-07-27 23:22:45 +0900

    feat: JSP/JSTL 뷰 지원 추가 및 war 패키징 전환

--- src/main/java/com/example/backend_lab/BackendLabApplication.java
+++ src/main/java/com/example/backend_lab/BackendLabApplication.java
@@ -2,12 +2,19 @@ package com.example.backend_lab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BackendLabApplication {
+public class BackendLabApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendLabApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BackendLabApplication.class);
 	}
 
 }
---
+++ src/main/java/com/example/backend_lab/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.backend_lab;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String Hello(Model model) {
+        model.addAttribute("message", "World");
+        return "hello";
+    }
+
+}

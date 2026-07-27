3ca87cfef1c9001970a9364d8c25191ce535545e
parkjieun87 <dksxlzhd0807@naver.com>
2026-07-27 22:55:58 +0900

    테스트 커밋

---
+++ src/test/java/com/example/backend_lab/BackendLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
